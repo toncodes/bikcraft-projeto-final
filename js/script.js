0389b28f6341de2eaa27dedb59ddc9476d2db4d0
toncodes <everton.vgs_liverock@hotmail.com>
2022-12-24 10:33:59 -0300

    js plugins

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ function ativarLink(link) {
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
-
 }
 
 links.forEach(ativarLink);
@@ -17,12 +16,57 @@ links.forEach(ativarLink);
 
 const parametros = new URLSearchParams(location.search);
 
-function ativarProduto(parametro){
+function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
-  if(elemento) {
+  if (elemento) {
     elemento.checked = true;
   }
- 
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  matchMedia("(min-width: 1000px)").matches;
+
+  if(media){
+    galeriaContainer.prepend(img);
+  }  
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//Animação
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
