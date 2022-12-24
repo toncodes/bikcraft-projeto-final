83a047c5c0a49f103332b9239d3db79963353684
toncodes <everton.vgs_liverock@hotmail.com>
2022-12-23 21:26:46 -0300

    parametros url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -11,3 +12,17 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro);
+  if(elemento) {
+    elemento.checked = true;
+  }
+ 
+}
+
+parametros.forEach(ativarProduto);
